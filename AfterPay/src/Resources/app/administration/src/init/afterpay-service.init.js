8e5fe7e9a9e307d4609f9e52f20b40a9b040e09d
colognation <80593468+colognation@users.noreply.github.com>
2021-05-21 20:47:25 +0200

    Delete AfterPay directory

--- AfterPay/src/Resources/app/administration/src/init/afterpay-service.init.js
+++
@@ -1,8 +0,0 @@
-import AfterPayService from '../service/afterpay.service.js';
-
-Shopware.Application.addServiceProvider('AfterPayService', container => {
-    return new AfterPayService(
-        Shopware.Application.getContainer('init').httpClient,
-        Shopware.Service('loginService')
-    );
-});
--- AfterPay/src/Resources/app/administration/src/main.js
+++
@@ -1,8 +0,0 @@
-import './init/afterpay-service.init.js';
-import './module/sw-order/page/sw-order-list';
-
-import deDE from './snippet/de-DE.json';
-import enGB from './snippet/en-GB.json';
-
-Shopware.Locale.extend('de-DE', deDE);
-Shopware.Locale.extend('en-GB', enGB);
