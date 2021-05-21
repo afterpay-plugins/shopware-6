370e2c16751ee17ef5bea135a322b54bbe64ed87
colognation <80593468+colognation@users.noreply.github.com>
2021-05-21 20:46:57 +0200

    Add files via upload

---
+++ AfterPay/src/Resources/snippet/de_DE/SnippetFile_de_DE.php
@@ -0,0 +1,33 @@
+<?php declare(strict_types=1);
+
+namespace Colo\AfterPay\Resources\snippet\de_DE;
+
+use Shopware\Core\System\Snippet\Files\SnippetFileInterface;
+
+class SnippetFile_de_DE implements SnippetFileInterface
+{
+    public function getName(): string
+    {
+        return 'snippets.de-DE';
+    }
+
+    public function getPath(): string
+    {
+        return __DIR__ . '/snippets.de-DE.json';
+    }
+
+    public function getIso(): string
+    {
+        return 'de-DE';
+    }
+
+    public function getAuthor(): string
+    {
+        return 'Arvato AfterPay';
+    }
+
+    public function isBase(): bool
+    {
+        return false;
+    }
+}
