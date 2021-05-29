9369b42552700c64941f669174f401f39249e467
colognation <80593468+colognation@users.noreply.github.com>
2021-05-29 20:40:00 +0200

    Delete src directory

--- src/Constants.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'urls' => [
-        'prod' => 'https://api.afterpay.io/api/v3/',
-        'public_sandbox' => 'https://sandbox.afterpay.io/api/v3/',
-        'sandbox' => 'https://sandboxapi.horizonafs.com/eCommerceServicesWebApi/api/v3/',
-    ],
-];
